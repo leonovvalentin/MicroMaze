53bb699923863c1aa9a0dd883b81ac80e41c6eb2
leonovvalentin <leonov.valentin@gmail.com>
2012-02-02 12:24:23 +0600

    Initial Commit

---
+++ MicroMaze/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MicroMaze
+//
+//  Created by admin on 02.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@end
---
+++ MicroMaze/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  MicroMaze
+//
+//  Created by admin on 02.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
